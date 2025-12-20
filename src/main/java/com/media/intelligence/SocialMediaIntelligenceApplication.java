f13acc472e6baa24350960ff16cab855752fb285
talhaahmed884 <talhaahmed884@gmail.com>
2025-12-20 07:21:17 -0800

    1. Initial commit

---
+++ src/main/java/com/media/intelligence/SocialMediaIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.media.intelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaIntelligenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialMediaIntelligenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/media/intelligence/SocialMediaIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media.intelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaIntelligenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
